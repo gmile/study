30997840c29a765bee9da5a8842d77df4181972a
gmile <iamexile@gmail.com>
2010-11-24 02:02:49 +0200

    Some progress

--- lexical_analyser.rb
+++ lexical_analyser.rb
@@ -9,36 +9,64 @@ class Token
 end
 
 class Parser
-  attr_reader :string
-  attr_reader :out
+  attr_writer :input
+  attr_reader :output
+
+  RESERVED_WORDS = [
+    'const',    'var',    'uses',
+    'begin',    'for',    'do',
+    'end',      'not',    'or',
+    'and',      'repeat', 'while',
+    'true',     'false',  'procedure',
+    'function', 'if',     'then',
+    'else'
+  ]
 
   FILTERS = {
+    :operations  => '[/+*()]',
     :user_data   => '\w+',
-    :assignement => '\+=',
-    :operations  => "[\/+*()]",
-    :quotes      => %Q{['"]}
+    :strings     => "\'.*\'",
+    :assignement => ':=',
   }
 
-  def initialize string
+  ERROR = {
+    :input_missing => 'Input string is not given'
+  }
+
+  def initialize string = nil
     @input = string
     @output = []
   end
 
   def tokenize
-    
+    # 1. check if reserved word
+    # 2. check else
+    @output.each do |token|
+      item = case token
+      when /#{}/  then 'Decimal'
+      when /\w+/ then 'Variable'
+      end
+    end
+  end
+
+  def valid?
+    unless @input.nil?
+      x = @output.map{|i| i.include?("'") ? i.delete(' ') : i}.join
+      y = @input.delete(' ')
+
+      x == y
+    else
+      ERROR[:input_missing]
+    end
   end
   
   def divide
-    # check for not valid sybols before
-    filter = Regexp.new FILTERS.values.join('|')
+    unless @input.nil?
+      filter = Regexp.new(FILTERS.values.join('|'))
 
-    @out = @input.scan filter
+      @output = @input.scan filter
+    else
+      ERROR[:input_missing]
+    end
   end
 end
-
-string = %Q{ x_ := 2 mod 0 + (10 + coma/12) * pjotr + "asdsad" + ' + ' }.strip
-
-parser = Parser.new(string)
-parser.divide
-
-puts parser.out.inspect
