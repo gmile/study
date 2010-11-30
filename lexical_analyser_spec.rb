54db01bc5d1da8a0cb925ee36e7bd4bfabc98922
gmile <iamexile@gmail.com>
2010-11-30 20:49:19 +0200

    Check minus sign

--- lexical_analyser_spec.rb
+++ lexical_analyser_spec.rb
@@ -39,10 +39,10 @@ describe Parser do
     @parser.output.should == ['x', ':=', "'Hello'", '+', "dear", '+', "'world!'"]
   end
 
-  it "should parse strings with real values: x := 2.3 + 4.6" do
-    @parser.input = "x := 2.3 + 4.6"
+  it "should parse strings with real values: x := 2.3 - 4.6" do
+    @parser.input = "x := 2.3 - 4.6"
     @parser.divide
-    @parser.output.should == ['x', ':=', "2.3", '+', "4.6"]
+    @parser.output.should == ['x', ':=', "2.3", '-', "4.6"]
   end
 
   it "should parse strings as strings: x := 'Hello' + 'dear' + 'world!'" do
