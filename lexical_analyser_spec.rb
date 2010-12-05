823f3c0acbfcc23bf9b33a50b292b858503c4eb3
gmile <iamexile@gmail.com>
2010-12-05 02:27:44 +0200

    First full source code test (wip)

--- lexical_analyser_spec.rb
+++ lexical_analyser_spec.rb
@@ -192,4 +192,32 @@ describe Parser do
       ]
     end
   end
+
+  context 'Multiline' do
+    it 'should correctly parse a full multiline program' do
+      source_code = <<-eos
+        uses crt;
+
+        var
+          my_var_1 : integer;
+          my_var_2 : real;
+          x        : string;
+
+        begin
+          clrscr;
+
+          my_var_1 = 12
+          while my_var_1 <> 0 do
+            dec(my_var_1);
+          end
+        end.
+      eos
+
+      @parser.input = source_code
+      @parser.divide
+      @parser.tokenize
+
+      # assert should be here
+    end
+  end
 end
