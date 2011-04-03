b0e9c606ea0c11ab2308492e4ef754e2908a20e1
gmile <iamexile@gmail.com>
2011-04-03 14:49:33 +0300

    Refactoring: rename variables

--- test.rb
+++ test.rb
@@ -1,6 +1,6 @@
-@a = ['program', 'test', ';']
+@dictionary = ['program', 'test', ';']
 
-@r = {
+@table = {
   :r1 => 'program' ,
   :r2 => 'test'    ,
   :r3 => ';'       ,
@@ -8,44 +8,43 @@
   :r5 => [:r1, :r2]
 }
 
-@r_k = @r.keys
+@nterminals = @table.keys
 
-n = @a.size
-r = @r.size
+n = @dictionary.size
+r = @table.size
+start_symbols = @table.select { |k,v| v.is_a?(Array) }
 
-@p = Hash.new
+matrix = Hash.new
 
 for i in 1..n do
   for j in 1..n do
     for k in 1..r do
-      @p[[i,j,k]] = false
+      matrix[[i,j,k]] = false
     end
   end
 end
 
 for i in 1..n do
-  x = @r_k.index { |key| @r[key] == @a[i-1] } + 1
-  @p[[i,1,x]] = true
+  x = @nterminals.index { |key| @table[key] == @dictionary[i-1] } + 1
+  matrix[[i,1,x]] = true
 end
-
-complex_rules = @r.select { |k,v| v.is_a?(Array) }
 
 for i in 2..n do
   for j in 1..n-i+1 do
     for k in 1..i-1 do
-      for rule in complex_rules
-        a = @r_k.index { |item| item == rule[0] } + 1
-        b = @r_k.index { |item| item == rule[1].first } + 1
-        c = @r_k.index { |item| item == rule[1].last } + 1
+      for rule in start_symbols
+        a = @nterminals.index { |item| item == rule[0] } + 1
+        b = @nterminals.index { |item| item == rule[1].first } + 1
+        c = @nterminals.index { |item| item == rule[1].last } + 1
 
-        @p[[j, i, a]] = true if @p[[j, k, b]] and @p[[j+k, i-k, c]] 
+        matrix[[j, i, a]] = true if matrix[[j, k, b]] and matrix[[j+k, i-k, c]]
       end
     end
   end
 end
 
-for i in @r_k.size+1-complex_rules.size..@r_k.size do
-  puts 'success!' if @p[[1,n,i]]
+for i in @nterminals.size+1-start_symbols.size..@nterminals.size do
+  puts 'success!' if matrix[[1,n,i]]
 end
 
 #R1 -> program
