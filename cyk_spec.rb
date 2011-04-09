6cd7c7802fefe989c37148a1926b9a22755532dd
gmile <iamexile@gmail.com>
2011-04-09 18:35:33 +0300

    Tests for start_symbols_from method

--- cyk_spec.rb
+++ cyk_spec.rb
@@ -30,7 +30,38 @@ describe Cyk do
     end
   end
 
-  it 'should parse a real piece of program' do
+  context "start_symbols_from" do
+    before :each do
+      @options = {
+        :string => 'program test ;'.split(' '),
+        :table => {
+          :r1 => ['program'],
+          :r2 => ['test'   ],
+          :r3 => [';'      ],
+          :r4 => [[:r1, :r3], [:r2, :r3]],
+          :r5 => [[:r1, :r2], [:r3, :r5]]
+        }
+      }
+    end
+
+    it "should find no start symbols" do
+      @options[:table].merge!({ :r4 => [[:r4, :r1]] })
+
+      Cyk.new(@options).start_symbols.should == Set.new
+    end
+
+    it "should find one start symbol" do
+      Cyk.new(@options).start_symbols.should == Set.new([:r4])
+    end
+
+    it "should find two start symbols" do
+      @options[:table].merge!({ :r6 => [[:r1, :r2]] })
+
+      Cyk.new(@options).start_symbols.should == Set.new([:r4, :r6])
+    end
+  end
+
+  it 'should parse numerical expression' do
     options = {
       :string => '1 + 2 / 3 + 5'.split(' '),
       :table  => {
