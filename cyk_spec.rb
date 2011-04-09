ba20e9892ff9ce9eeaa63f8b25d39fa8f8c82aea
gmile <iamexile@gmail.com>
2011-04-09 19:44:01 +0300

    A bunch of tests

--- cyk_spec.rb
+++ cyk_spec.rb
@@ -1,4 +1,5 @@
 require_relative 'cyk'
+require_relative 'lexical_analyser'
 
 describe Cyk do
   context '"program test ;"' do
@@ -23,7 +24,7 @@ describe Cyk do
       @options[:table].merge!({
         :r4 => [[:r5, :r3], [:r2, :r3]],
         :r5 => [[:r1, :r2], [:r2, :r1]],
-        :r6 => [[:r7, :r8]]
+        :r6 => ['2', [:r1, :r1]]
       })
 
       Cyk.new(@options).valid?.should be_true
@@ -70,7 +71,72 @@ describe Cyk do
         :r3 => ['+', '-', '*', '/']
       }
     }
-
+    # no start symbols here...
     Cyk.new(options).valid?.should be_true
+  end
+
+  it 'should parse with a rule: R -> A A' do
+    @options = {
+      :string => 'a b c d'.split(' '),
+      :table  => {
+        :r1 => ['a'],
+        :r2 => ['b'],
+        :r3 => ['c'],
+        :r4 => ['d'],
+        :r5 => [[:r1, :r2], [:r3, :r4]],
+        :r6 => [[:r5, :r5]]
+      }
+    }
+
+    x = Cyk.new(@options)
+    result = x.valid?
+    result.should be_true
+  end
+
+  it 'should parse with a rule: R -> A A' do
+    @options = {
+      :string => 'a b c d a b c d a b c d a'.split(' '),
+      :table  => {
+        :r1 => ['a'],
+        :r2 => ['b'],
+        :c1 => ['c'],
+        :c2 => ['d'],
+        :r3 => [[:r1, :r2], [:c1, :c2], [:r3, :r3]],
+        :r4 => [[:r3, :r1]]
+      }
+    }
+
+    Cyk.new(@options).valid?.should be_true
+  end
+
+  it 'should parse a real piece of program with reccurent produtions' do
+    source_code = <<-eos
+      s := 10; s := 12; s := 14;
+    eos
+
+    @parser = Parser.new(source_code)
+    @parser.divide
+    @parser.tokenize
+
+    string = @parser.output.flatten.map { |token| token.type }
+
+    options = {
+      :string => string,
+      :table  => {
+        :t1  => [:user_data],
+        :t2  => [:assignement],
+        :t3  => [:number],
+        :t4  => [:semi],
+        :e1  => [[:t1, :t2]],
+        :e2  => [[:e1, :t3]],
+        :e3  => [[:e2, :t4]],
+        :e4  => [[:e3, :e3]],
+        :e5  => [[:e4, :e3]]
+      }
+    }
+
+    x = Cyk.new(options)
+    result = x.valid?
+    result.should be_true
   end
 end
