08e6987660de30b000d644fe23b84b21b34265cd
gmile <iamexile@gmail.com>
2011-04-24 21:18:52 +0300

    Remove Exception test

--- spec/cnf_table_spec.rb
+++ spec/cnf_table_spec.rb
@@ -2,33 +2,35 @@ require_relative '../cnf_table'
 require_relative '../cyk'
 
 describe Cyk do
-  it "should raise error if no A -> B C rules given" do
-    CNFTable.stub(:non_terminals).and_return({})
+  let!(:table) { CNFTable.table }
 
+  it "should raise error if no A -> B C rules given" do
     string = [:program, :variable, :semicolon]
 
-    lambda { Cyk.new(string, CNFTable.table) }.should raise_error(Cyk::NoPairProductionsException, 'No A -> BC productions given. Have you specified them?')
+    Cyk.new(string, table).valid?.should be_true
   end
 
-  context 'CNF Table' do
-    let!(:table) { CNFTable.table }
+  it "should parse 'uses' block with only one arg" do
+    string = [:uses, :variable, :semicolon]
 
-    it "should raise error if no A -> B C rules given" do
-      string = [:program, :variable, :semicolon]
+    Cyk.new(string, table).valid?.should be_true
+  end
 
-      Cyk.new(string, table).valid?.should be_true
-    end
+  it "should parse 'uses' block with several args" do
+    string = [:uses, :variable, :coma, :variable, :semicolon]
 
-    it "should parse 'uses' block with only one arg" do
-      string = [:uses, :variable, :semicolon]
+    Cyk.new(string, table).valid?.should be_true
+  end
 
-      Cyk.new(string, table).valid?.should be_true
-    end
+  it "should parse 'begin' block without inner values" do
+    string = [:begin, :end]
 
-    it "should parse 'uses' block with several args" do
-      string = [:uses, :variable, :coma, :variable, :semicolon]
+    Cyk.new(string, table).valid?.should be_true
+  end
 
-      Cyk.new(string, table).valid?.should be_true
-    end
+  it "should parse 'begin' block without inner values" do
+    string = [:variable, :assignement, :variable, :add, :variable]
+
+    Cyk.new(string, table).valid?.should be_true
   end
 end
