117912486ce941e9a5ef9625fe1ebc7a49b5a442
gmile <iamexile@gmail.com>
2011-04-24 21:18:18 +0300

    Expand table a bit more

--- cnf_table.rb
+++ cnf_table.rb
@@ -9,13 +9,23 @@ class CNFTable
 
   def self.non_terminals
     {
-      :var_and_coma      => [[:n_variable,   :n_coma]],
-      :var_and_semicolon => [[:n_variable,   :n_semicolon]],
+      :var_and_coma      => [[:n_variable,    :n_coma           ]],
+      :var_and_semicolon => [[:n_variable,    :n_semicolon      ]],
 
-      :program_fold      => [[:n_program,    :var_and_semicolon]],
+      :program_fold      => [[:n_program,     :var_and_semicolon]],
 
-      :uses_fold         => [[:n_uses,       :var_and_semicolon], [:n_uses,       :uses_fold_1]],
-      :uses_fold_1       => [[:var_and_coma, :var_and_semicolon], [:var_and_coma, :uses_fold_1]]
+      :uses_fold         => [[:n_uses,        :var_and_semicolon], [:n_uses,       :uses_fold_1]],
+      :uses_fold_1       => [[:var_and_coma,  :var_and_semicolon], [:var_and_coma, :uses_fold_1]],
+
+      :block_fold        => [[:n_begin,       :n_end            ]],
+
+      :operation         => [:add, :sub, :mul, :div              ],
+      :value             => [:integer, :real, :variable          ],
+      :value_fold        => [[:value_fold_1,  :value            ]],
+      :value_fold_1      => [[:value,         :operation        ]],
+
+      :common_expr       => [[:n_variable,    :common_expr_1    ]],
+      :common_expr_1     => [[:n_assignement, :value_fold       ]]
     }
   end
 
