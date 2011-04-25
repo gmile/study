f5c8b09f8984f64dc664d6ba16a18b5b911a7765
gmile <iamexile@gmail.com>
2011-04-25 20:31:59 +0300

    Reorganize tests

--- spec/cnf_table_spec.rb
+++ spec/cnf_table_spec.rb
@@ -4,33 +4,91 @@ require_relative '../cyk'
 describe Cyk do
   let!(:table) { CNFTable.table }
 
-  it "should raise error if no A -> B C rules given" do
-    string = [:program, :variable, :semicolon]
+  it 'program_title_fold' do
+    string = [:program, :variable]
 
-    Cyk.new(string, table).valid?.should be_true
+    cyk = Cyk.new(string, table)
+    cyk.start_symbols << :program_title_fold
+
+    cyk.valid?.should be_true
+    cyk.parse_tree.last.last.should == :program_title_fold
   end
 
-  it "should parse 'uses' block with only one arg" do
-    string = [:uses, :variable, :semicolon]
+  context 'uses_fold' do
+    it 'with only one arg' do
+      string = [:uses, :variable]
 
-    Cyk.new(string, table).valid?.should be_true
+      cyk = Cyk.new(string, table)
+      cyk.start_symbols << :uses_fold
+
+      cyk.valid?.should be_true
+      cyk.parse_tree.last.last.should == :uses_fold
+    end
+
+    it 'with several args' do
+      string = [:uses, :variable, :coma, :variable]
+
+      cyk = Cyk.new(string, table)
+      cyk.start_symbols << :uses_fold
+
+      cyk.valid?.should be_true
+      cyk.parse_tree.last.last.should == :uses_fold
+    end
   end
 
-  it "should parse 'uses' block with several args" do
-    string = [:uses, :variable, :coma, :variable, :semicolon]
+  it 'value fold' do
+    string = [:variable, :add, :real, :add, :integer, :add, :variable]
 
-    Cyk.new(string, table).valid?.should be_true
+    cyk = Cyk.new(string, table)
+    cyk.start_symbols << :value_fold
+
+    cyk.valid?.should be_true
+    cyk.parse_tree.last.last.should == :value_fold
   end
 
-  it "should parse 'begin' block without inner values" do
-    string = [:begin, :end]
+  context 'common_expr_fold' do
+    it 'simple right side' do
+      string = [:variable, :assignement, :variable]
 
-    Cyk.new(string, table).valid?.should be_true
+      cyk = Cyk.new(string, table)
+      cyk.start_symbols << :common_expr_fold
+
+      cyk.valid?.should be_true
+      cyk.parse_tree.last.last.should == :common_expr_fold
+    end
+
+    it 'complex right side' do
+      string = [:variable, :assignement, :variable, :add, :variable, :add, :integer]
+
+      cyk = Cyk.new(string, table)
+      cyk.start_symbols << :common_expr_fold
+
+      cyk.valid?.should be_true
+      cyk.parse_tree.last.last.should == :common_expr_fold
+    end
   end
 
-  it "should parse 'begin' block without inner values" do
-    string = [:variable, :assignement, :variable, :add, :variable]
+  context 'block_fold' do
+    it 'with no statements' do
+      string = [:begin, :end]
 
-    Cyk.new(string, table).valid?.should be_true
+      cyk = Cyk.new(string, table)
+      cyk.start_symbols << :block_fold
+
+      cyk.valid?.should be_true
+      cyk.parse_tree.last.last.should == :block_fold
+    end
+
+    it 'with one statement' do
+      string = [:begin, :variable, :assignement, :variable, :add, :variable, :add, :integer, :end]
+
+      cyk = Cyk.new(string, table)
+      cyk.start_symbols << :block_fold
+
+      cyk.valid?.should be_true
+      cyk.parse_tree.last.last.should == :block_fold
+    end
+
+    it 'with several statements'
   end
 end
