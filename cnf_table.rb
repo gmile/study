ed4326bcf7c5d57afbd1db2ef7964d9b0ee92247
gmile <iamexile@gmail.com>
2011-04-26 01:24:41 +0300

    Refresh table

--- cnf_table.rb
+++ cnf_table.rb
@@ -9,20 +9,19 @@ class CNFTable
 
   def self.non_terminals
     {
-      :operation          => [:add, :sub, :mul, :div    ],
-      :value              => [:integer, :real, :variable],
+      :operation             => [:add, :sub, :mul, :div    ],
+      :value                 => [:integer, :real, :variable],
 
-      :program_title_fold => [[:n_program,  :n_variable]],
-      :uses_fold          => [[:n_uses,     :n_variable], [:n_uses, :uses_fold_1]],
-      :uses_fold_1        => [[:n_variable, :n_coma],     [:uses_fold_1, :n_variable], [:uses_fold_1, :uses_fold_1]],
+      :program_title_fold    => [[:n_program,  :n_variable]],
+      :uses_fold             => [[:n_uses, :n_variable], [:n_uses, :uses_fold], [:n_variable, :uses_fold], [:n_coma, :uses_fold], [:n_coma, :n_variable]],
+      :value_fold            => [[:value, :operation], [:value_fold, :value], [:value_fold, :value_fold]],
 
-      :value_fold         => [[:value,      :operation], [:value_fold, :value], [:value_fold, :value_fold]],
+      :common_expr_fold      => [[:n_variable, :common_expr_fold_1]], # can we somehow put value and value_fold toather?
+      :common_expr_fold_1    => [[:n_assignement, :value_fold], [:n_assignement, :value]],
 
-      :common_expr_fold   => [[:n_variable,    :common_expr_fold_1    ]],
-      :common_expr_fold_1 => [[:n_assignement, :value_fold       ], [:n_assignement, :value]],
+      :common_expr_fold_list => [[:common_expr_fold_list, :common_expr_fold_list], [:common_expr_fold, :n_semicolon], [:common_expr_fold_list, :common_expr_fold]],
 
-      :block_fold         => [[:n_begin, :n_end], [:n_begin, :block_fold_1]],
-      :block_fold_1       => [[:common_expr_fold, :n_end]]
+      :block_fold            => [[:n_begin, :n_end], [:n_begin, :block_fold], [:common_expr_fold, :n_end], [:common_expr_fold_list, :n_end]]
     }
   end
 
